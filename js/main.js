925d4eb4da75d3be691059c9512fd83f14836adc
Andrii Nikulin <nikulin.and90@gmail.com>
2023-07-28 17:15:24 +0300

    I define variables that receive links to various elements of the page

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+
+const mainTableBody = document.querySelector('tbody.main-table-body');
+const mainTableHead = document.querySelector('table.main-table thead');
+const deleteAllNotesBtn = mainTableHead.querySelector('.fas.fa-trash');
+const archiveAllNotesBtn = mainTableHead.querySelector('.fas.fa-archive');
+const createNoteBtn = document.querySelector('.button-create');
+const sumTableBtns = document.querySelectorAll('.head.note-item.btn');
